89bf0377e6216b8c5f8041582b6206315dce3c6c
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-04-07 15:39:09 +0300

    added Arithmetic method

---
+++ src/main/java/com/kondratiev/Arithmetic.java
@@ -0,0 +1,19 @@
+package com.kondratiev;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 38;
+        int c = 54;
+        int d = 86;
+
+        System.out.println("a + b =" + (a + b));
+        System.out.println("b - a =" + (a - b));
+        System.out.println("a * b =" + (a * b));
+        System.out.println("d / c =" + (d / c));
+        System.out.println("d % c =" + (d % c));
+        System.out.println("a % b =" + (a % b));
+
+    }
+}
