41f5aae8b45a130f2413f9acb2ba2e1b04138521
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-04-07 16:12:14 +0300

    added LogicalOperator method

---
+++ src/main/java/com/kondratiev/LogicalOperator.java
@@ -0,0 +1,10 @@
+package com.kondratiev;
+
+public class LogicalOperator {
+
+    public static void main(String[] args) {
+        int age = 33;
+        String sex = "Male";
+        boolean canDrinkAlcohol = age >= 18 & sex.equals("Male");
+    }
+}
