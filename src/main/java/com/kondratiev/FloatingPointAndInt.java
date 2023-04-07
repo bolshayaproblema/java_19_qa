a4f46e91b2cb81b7c849774b2e3d6cb49c9aed88
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-04-07 15:40:50 +0300

    added Arithmetic method

---
+++ src/main/java/com/kondratiev/FloatingPointAndInt.java
@@ -0,0 +1,20 @@
+package com.kondratiev;
+
+public class FloatingPointAndInt {
+
+
+    public static void main(String[] args) {
+
+
+        int a = 80;
+        double b = 12.142;
+        int c = 125;
+        double d = 63.121;
+        int e = 510;
+        double f = 31.8612;
+
+        System.out.println("a + b =" + (a + b));
+        System.out.println("c * d =" + (c * d));
+        System.out.println("e / f =" + (e / f));
+    }
+}
