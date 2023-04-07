dbe0d3fd00ecb214a3b82e4f8160330baeace449
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-04-07 15:55:10 +0300

    added IfElse method

---
+++ src/main/java/com/kondratiev/IfElse.java
@@ -0,0 +1,18 @@
+package com.kondratiev;
+
+public class IfElse {
+
+    public static void main(String[] args) {
+
+        int a = 50;
+        int b = 80;
+        if (a > b) {
+
+            System.out.println("a + b = " + (a + b));
+        }
+        else {
+            System.out.println("b + a = " + (b + a));
+
+        }
+    }
+}
