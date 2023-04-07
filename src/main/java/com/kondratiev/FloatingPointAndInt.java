bb38480b5d4d70a95e94ac0582a3fb32cd588ef1
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-04-07 15:44:07 +0300

    added FloatingPointAndInt method

--- src/main/java/com/kondratiev/FloatingPointAndInt.java
+++ src/main/java/com/kondratiev/FloatingPointAndInt.java
@@ -2,19 +2,18 @@ package com.kondratiev;
 
 public class FloatingPointAndInt {
 
-
     public static void main(String[] args) {
 
+            int a = 80;
+            double b = 12.142;
+            int c = 125;
+            double d = 63.121;
+            int e = 510;
+            double f = 31.8612;
 
-        int a = 80;
-        double b = 12.142;
-        int c = 125;
-        double d = 63.121;
-        int e = 510;
-        double f = 31.8612;
+            System.out.println("a + b =" + (a + b));
+            System.out.println("c * d =" + (c*d));
+            System.out.println("e / f =" + (e/f));
 
-        System.out.println("a + b =" + (a + b));
-        System.out.println("c * d =" + (c * d));
-        System.out.println("e / f =" + (e / f));
     }
 }
