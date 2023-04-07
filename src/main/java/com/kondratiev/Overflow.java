9271e6cdbf5ee622918200e1c4b7b56d7427c0ff
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-04-07 16:12:39 +0300

    added Overflow method

---
+++ src/main/java/com/kondratiev/Overflow.java
@@ -0,0 +1,12 @@
+package com.kondratiev;
+
+public class Overflow {
+
+    public static void main(String[] args) {
+
+
+        int a = 2147483647;
+        int b = 121125;
+        System.out.println("a + b ="+ (a + b));
+    }
+}
